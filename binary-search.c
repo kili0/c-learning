9949498467369785b18c2d3cd366a93ea02f6fc8
kili0 <52195771+kili0@users.noreply.github.com>
2020-05-19 15:29:59 +0900

    Update binary-search.c

--- binary-search.c
+++ binary-search.c
@@ -3,40 +3,49 @@
 #include <time.h>
 
 int GetRandom(int min, int max);
-int binary_search(int array[], double ok, double ng, int k);
+int binary_search(int array[], int ok, int ng, int k);
 
 int main(void) {
   int n = 1000000;
   int array[n];
 
-  array[0] = 3;
+  array[0] = GetRandom(1, 10);
   srand((unsigned int)time(NULL));
   for(int i=0; i<n-1; i++) {
     array[i+1] = array[i] + GetRandom(1, 10);
   }
 
-  int key = array[GetRandom(0, n)];
+  //------test------
+  /*for(int i=0; i<n; i++) {
+    int key = array[i];
+    int result = (binary_search(array, 0.0, n, key)) + 1;
 
-  int result = (binary_search(array, 0.0, n, key)) + 1;
+    if(result > 0) {
+
+    } else {
+      printf("%dは配列に存在しません\n", key);
+    }
+  }*/
+  //----------------
+
+  int key = array[GetRandom(0, n)];
+  int result = (binary_search(array, 0, n, key)) + 1;
 
   if(result > 0) {
     printf("%dは配列の%d番目にあります\n", key, result);
   } else {
     printf("%dは配列に存在しません\n", key);
   }
-  //for(int i=0; i<n; i++){
-  //  printf("%d\n", array[i]);
-  //}
+  /*for(int i=0; i<n; i++){
+    printf("%d\n", array[i]);
+  } */
 
   return 0;
 }
 
-int binary_search(int array[], double ok, double ng, int k) {
-  int mid;
+int binary_search(int array[], int ok, int ng, int k) {
   if(ok <= ng) {
-    for(int i=0; i<100; i++){
-      mid = (ok + ng) / 2.0;
-    }
+    int mid = (ok + ng) / 2;
 
     if(array[mid] == k) {
       return mid;
@@ -54,3 +63,4 @@ int binary_search(int array[], double ok, double ng, int k) {
 int GetRandom(int min,int max){
 	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
 }
+
